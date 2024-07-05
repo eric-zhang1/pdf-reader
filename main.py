db91318775051c4c054bcdf94c2ad7ed3123ce64
eric-zhang1 <ericletianzhang@gmail.com>
2024-07-05 17:41:58 -0400

    poppler downloaded and file path corrected

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import streamlit as st
 import platform
 from tempfile import TemporaryDirectory
 from pathlib import Path
+import os
 
 import pytesseract
 from pdf2image import convert_from_path
-from pypdf import PdfMerger
+from pypdf import PdfWriter
 
 image_file_list = []
-
-# TODO: Replace PdfMerger with PDFReader + PDFWriter. Might need to look into pypdf
 
 if platform.system() == "Windows":
 	# We may need to do some additional downloading and setup...
@@ -22,7 +21,7 @@ if platform.system() == "Windows":
 	)
 
 	# Windows also needs poppler_exe
-	path_to_poppler_exe = Path(r"C:\.....")
+	path_to_poppler_exe = Path(r"C:\Program Files\poppler-24.02.0\Library\bin")
 
 def main():
     st.title("Pdf Reader")
@@ -34,16 +33,20 @@ def main():
     uploaded_file = st.sidebar.file_uploader("Choose a pdf file to ocr")
     if uploaded_file is not None:
         with TemporaryDirectory() as tempdir:
+            # copy the file to a temp folder
+            file_path = os.path.join(tempdir, uploaded_file.name)
+            with open(file_path, "wb") as f:
+                f.write(uploaded_file.getvalue())
             # Convert all pages to images
             if platform.system() == "Windows":
                 pdf_pages = convert_from_path(
-					uploaded_file, 500, poppler_path=path_to_poppler_exe
+					file_path, 500, poppler_path=path_to_poppler_exe
 				)
             else:
-                pdf_pages = convert_from_path(uploaded_file, 500)
+                pdf_pages = convert_from_path(file_path, 500)
             
-			# Create pdf merger
-            merger = PdfMerger()
+			# Create pdf writer
+            merger = PdfWriter()
             
 			# Loop through each page of the pdf
             for page_enumeration, page in enumerate(pdf_pages, start=1):
@@ -53,17 +56,26 @@ def main():
 				# Convert each page to an image
                 page.save(filenameJPG, "JPEG")
                 
-            	# Convert each image to searchable pdf
+                # Convert each image to searchable pdf
                 current_page = pytesseract.image_to_pdf_or_hocr(filenameJPG, extension="pdf")
                 with open(filenamePDF, "w+b") as f:
                     f.write(current_page)
                 
 				# Append current page to the end of pdf
                 merger.append(filenamePDF)
-            merger.write(f"{uploaded_file.name}.pdf")
-        # Output converted pdf file
+            # Write the pdf to the temporary directory for download purposes
+            merger.write(f"{tempdir}\{uploaded_file.name}.pdf")
+            merger.close()
+            # Create download button for file.
+            with open(f"{tempdir}\{uploaded_file.name}.pdf", "rb") as file:
+                st.sidebar.download_button(
+                    label=f"Download {uploaded_file.name}",
+                    data=file,
+                    file_name=f"{uploaded_file.name}.pdf",
+                    mime="application/pdf"
+                )
+        
     
-
 
 
 if __name__ == "__main__":
